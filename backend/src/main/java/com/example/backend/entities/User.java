08a99dbc213712bfdcc396aca68de55741c06ba1
Tarun S Maidur <tarunmaidur@gmail.com>
2025-08-06 04:43:50 +0530

    completed

--- backend/src/main/java/com/example/backend/entities/User.java
+++ backend/src/main/java/com/example/backend/entities/User.java
@@ -2,8 +2,6 @@ package com.example.backend.entities;
 
 import java.util.Date;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
